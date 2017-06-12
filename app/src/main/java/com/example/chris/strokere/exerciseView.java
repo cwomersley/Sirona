010091631870f14c81c57ab00870c3c68535a23f
Chris <christopherwomersley@gmail.com>
2017-06-12 17:22:19 +0100

    added menu toolbar

--- app/src/main/java/com/example/chris/strokere/exerciseView.java
+++ app/src/main/java/com/example/chris/strokere/exerciseView.java
@@ -2,6 +2,8 @@ package com.example.chris.strokere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class exerciseView extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class exerciseView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_view);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
     }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
+
