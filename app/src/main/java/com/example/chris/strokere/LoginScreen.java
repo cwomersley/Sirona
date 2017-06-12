438b2c8117e283468eb47bd11cca53fd9ca0382e
Christopher Womersley <Chris@eduD316.kent.ac.uk>
2017-06-12 16:31:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/chris/strokere/LoginScreen.java
@@ -0,0 +1,25 @@
+package com.example.chris.strokere;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //Listner for not registered button to go to register screen
+        Button notRegisteredBtn = (Button) findViewById(R.id.notRegistered);
+        notRegisteredBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(LoginScreen.this, Register.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/chris/strokere/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.chris.strokere;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Listner for Log in button to go to log inscreen
+        Button logInBtn = (Button) findViewById(R.id.logInBtn);
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               startActivity(new Intent(MainActivity.this, LoginScreen.class));
+            }
+        });
+
+        //Listner for Log in button to go to Register screen
+        Button registerBtn = (Button) findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Register.class));
+            }
+        });
+    }
+}
