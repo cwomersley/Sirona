5c973e1fbbd6c7dcc3a3465664ebd0ec7531b75b
SJWard <samjward4@gmail.com>
2017-07-06 15:52:04 +0100

    Added a system to change calendar information by looping through a hashmap

--- app/src/main/java/com/example/chris/strokere/Progress.java
+++ app/src/main/java/com/example/chris/strokere/Progress.java
@@ -3,25 +3,24 @@ package com.example.chris.strokere;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.util.Map;
 import java.util.HashMap;
+
 
 public class Progress extends AppCompatActivity {
 
-    public HashMap<String,Boolean> days = new HashMap<String,Boolean>();
+    public HashMap<String,Integer> days = new HashMap<String,Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
-
-        days.put("Mon",false);
-        days.put("Tue",false);
-        days.put("Wed",false);
 
 
         TextView mon = (TextView) findViewById(R.id.monP);
@@ -44,28 +43,50 @@ public class Progress extends AppCompatActivity {
 
     }
 
-    public boolean randomBoolean(){
-        return Math.random() < 0.5;
+    public int randomNum(){
+        if(Math.random() < 0.5==true) {
+            return 2;
+        }
+        else {
+            return 1;
+        }
     }
+
 
     public void redrawDays() {
 
-        days.put("Mon",randomBoolean());
-        days.put("Tue",false);
-        days.put("Wed",false);
-        days.put("Thu",false);
-        days.put("Fri",false);
-        days.put("Sat",false);
-        days.put("Sun",false);
+        days.put("monP",randomNum());
+        days.put("tueP",randomNum());
+        days.put("wedP",randomNum());
+        days.put("thuP",randomNum());
+        days.put("friP",randomNum());
+        days.put("satP",randomNum());
+        days.put("sunP",randomNum());
 
-        if(days.get("Mon")==true) {
+        for(Map.Entry<String, Integer> entry : days.entrySet())
+        {
+            int id = getResources().getIdentifier(entry.getKey(), "id", getPackageName());
+            TextView textView = (TextView) findViewById(id);
+            Log.d("Logthis   ","Key = " + entry.getKey() + ", Value = " + entry.getValue() + id);
+            if(entry.getValue()==2) {
+                textView.setBackgroundColor(Color.parseColor("#4BAA71"));
+            }
+            if(entry.getValue()==1) {
+                textView.setBackgroundColor(Color.parseColor("#AAAAAA"));
+            }
+        }
+
+
+
+        /*if(days.get("Mon")==true) {
             TextView mon = (TextView) findViewById(R.id.monP);
             mon.setBackgroundColor(Color.BLUE);
         }
         else {
             TextView mon = (TextView) findViewById(R.id.monP);
             mon.setBackgroundColor(Color.parseColor("#FF4081"));
-        }
+        }*/
+
 
     }
 
