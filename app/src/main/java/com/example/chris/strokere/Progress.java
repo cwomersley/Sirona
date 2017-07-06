32934042ca4764370eccf1ce22710a8ca9136780
SJWard <samjward4@gmail.com>
2017-07-06 18:45:04 +0100

    Created drawables for the calendar day displays. Extended calendar to four weeks rather than one.

--- app/src/main/java/com/example/chris/strokere/Progress.java
+++ app/src/main/java/com/example/chris/strokere/Progress.java
@@ -30,13 +30,13 @@ public class Progress extends AppCompatActivity {
         TextView titleP = (TextView) findViewById(R.id.titleP);
         titleP.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
 
-        TextView mon = (TextView) findViewById(R.id.monP);
+        TextView mon = (TextView) findViewById(R.id.mon1P);
         mon.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
 
-        TextView tue = (TextView) findViewById(R.id.tueP);
+        TextView tue = (TextView) findViewById(R.id.tue1P);
         tue.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
 
-        TextView wed = (TextView) findViewById(R.id.wedP);
+        TextView wed = (TextView) findViewById(R.id.wed1P);
         wed.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
 
         Button shuffle = (Button) findViewById(R.id.shuffle);
@@ -62,13 +62,36 @@ public class Progress extends AppCompatActivity {
 
     public void redrawDays() {
 
-        days.put("monP",randomNum());
-        days.put("tueP",randomNum());
-        days.put("wedP",randomNum());
-        days.put("thuP",randomNum());
-        days.put("friP",randomNum());
-        days.put("satP",randomNum());
-        days.put("sunP",randomNum());
+        //This section models input from the database with a random number instead
+        days.put("mon1P",randomNum());
+        days.put("mon2P",randomNum());
+        days.put("mon3P",randomNum());
+        days.put("mon4P",randomNum());
+        days.put("tue1P",randomNum());
+        days.put("tue2P",randomNum());
+        days.put("tue3P",randomNum());
+        days.put("tue4P",randomNum());
+        days.put("wed1P",randomNum());
+        days.put("wed2P",randomNum());
+        days.put("wed3P",randomNum());
+        days.put("wed4P",randomNum());
+        days.put("thu1P",randomNum());
+        days.put("thu2P",randomNum());
+        days.put("thu3P",randomNum());
+        days.put("thu4P",randomNum());
+        days.put("fri1P",randomNum());
+        days.put("fri2P",randomNum());
+        days.put("fri3P",randomNum());
+        days.put("fri4P",randomNum());
+        days.put("sat1P",randomNum());
+        days.put("sat2P",randomNum());
+        days.put("sat3P",randomNum());
+        days.put("sat4P",randomNum());
+        days.put("sun1P",randomNum());
+        days.put("sun2P",randomNum());
+        days.put("sun3P",randomNum());
+        days.put("sun4P",randomNum());
+
 
         for(Map.Entry<String, Integer> entry : days.entrySet())
         {
@@ -76,10 +99,13 @@ public class Progress extends AppCompatActivity {
             TextView textView = (TextView) findViewById(id);
             Log.d("Logthis   ","Key = " + entry.getKey() + ", Value = " + entry.getValue() + id);
             if(entry.getValue()==2) {
-                textView.setBackgroundColor(Color.parseColor("#4BAA71"));
+                textView.setBackground(getResources().getDrawable(R.drawable.calendar_day_done));
+                //textView.setBackgroundColor(Color.parseColor("#4BAA71"));
+
             }
             if(entry.getValue()==1) {
-                textView.setBackgroundColor(Color.parseColor("#BABABA"));
+                textView.setBackground(getResources().getDrawable(R.drawable.calendar_day));
+                //textView.setBackgroundColor(Color.parseColor("#BABABA"));
             }
         }
 
