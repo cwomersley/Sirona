9877002eee7efd927700a9ee6d23f62be2d41ccf
weaselsnak <edward_boyles@hotmail.com>
2017-07-21 14:34:12 +0100

    Updated test for Mainactivity which checks logging in. First checks that Home activity is opened upon entering in correct credentials (test@test.com and password: password) And then checks that if the back button is pressed it exits the app.
    Also checks to see what happens if an incorrect password is entered, i.e home activity should not be open/user should not be logged in a toast is displayed to the user

--- app/src/androidTest/java/com/example/chris/strokere/MainActivityTest.java
+++ app/src/androidTest/java/com/example/chris/strokere/MainActivityTest.java
@@ -11,23 +11,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
+@LargeTest
 @RunWith(AndroidJUnit4.class)
-public class LoginTest {
+public class MainActivityTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void loginTest() {
+    public void mainActivityTest() {
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
@@ -39,39 +40,47 @@ public class LoginTest {
 
         ViewInteraction appCompatEditText = onView(
                 allOf(withId(R.id.emailL), isDisplayed()));
-        appCompatEditText.perform(replaceText("ed@ed.com"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText("test@test.com"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText2 = onView(
                 allOf(withId(R.id.passwordL), isDisplayed()));
-        appCompatEditText2.perform(replaceText("aaaaaa"), closeSoftKeyboard());
+        appCompatEditText2.perform(replaceText("password"), closeSoftKeyboard());
 
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.loginBtnM), withText("Log in"), isDisplayed()));
         appCompatButton.perform(click());
 
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3553565);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pressBack();
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3591143);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.emailL), withText("ed@ed.com"), isDisplayed()));
+                allOf(withId(R.id.emailL), isDisplayed()));
         appCompatEditText3.perform(replaceText("test@test.com"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText4 = onView(
-                allOf(withId(R.id.emailL), withText("test@test.com"), isDisplayed()));
-        appCompatEditText4.perform(pressImeActionButton());
-
-        ViewInteraction appCompatEditText5 = onView(
-                allOf(withId(R.id.passwordL), withText("aaaaaa"), isDisplayed()));
-        appCompatEditText5.perform(pressImeActionButton());
+                allOf(withId(R.id.passwordL), isDisplayed()));
+        appCompatEditText4.perform(replaceText("fgjtir"), closeSoftKeyboard());
 
         ViewInteraction appCompatButton2 = onView(
                 allOf(withId(R.id.loginBtnM), withText("Log in"), isDisplayed()));
         appCompatButton2.perform(click());
-
-        ViewInteraction appCompatButton3 = onView(
-                allOf(withId(R.id.loginBtnM), withText("Log in"), isDisplayed()));
-        appCompatButton3.perform(click());
-
-        ViewInteraction appCompatButton4 = onView(
-                allOf(withId(R.id.loginBtnM), withText("Log in"), isDisplayed()));
-        appCompatButton4.perform(click());
 
     }
 
