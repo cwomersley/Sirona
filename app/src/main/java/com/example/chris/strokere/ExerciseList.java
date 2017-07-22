b6c3f8eb5a9cb0b44f34638d9fc1875f6711b2be
aj2109 <adamj2109@gmail.com>
2017-07-22 13:41:10 +0100

    Searchbar for exercises, and list of exercises from array

--- app/src/main/java/com/example/chris/strokere/ExerciseList.java
+++ app/src/main/java/com/example/chris/strokere/ExerciseList.java
@@ -2,16 +2,62 @@ package com.example.chris.strokere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.text.Editable;
 
 public class ExerciseList extends AppCompatActivity {
 
-    public void showExercises(){
 
-    }
+    ListView listView;
+    ArrayAdapter adapter;
+    EditText inputSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_list);
+
+        List<String> list = new ArrayList<>();
+        list.add("exampleExercise1");
+        list.add("exampleExercise2");
+        list.add("exampleExercise3");
+        list.add("exampleExercise4");
+        list.add("exampleExercise5");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+
+        listView = (ListView) findViewById(R.id.list);
+
+
+       inputSearch = (EditText) findViewById(R.id.exerciseSearch);
+
+        listView.setAdapter(adapter);
+
+
+         inputSearch.addTextChangedListener(new TextWatcher() {
+
+
+            @Override
+            public void onTextChanged(CharSequence charSeq, int arg1, int arg2, int arg3) {
+                ExerciseList.this.adapter.getFilter().filter(charSeq);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+
+            }
+            });
+
+
+        }
     }
-}
