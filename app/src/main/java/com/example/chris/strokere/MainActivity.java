42c7816da9a96d96d3e1e6c52bedac0d28c4dfae
weaselsnak <edward_boyles@hotmail.com>
2017-07-25 14:59:19 +0100

    Be able to advance straight to home activity if user is already logged in

--- app/src/main/java/com/example/chris/strokere/MainActivity.java
+++ app/src/main/java/com/example/chris/strokere/MainActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity  {
 
@@ -32,32 +33,52 @@ public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        progressDialog = new ProgressDialog(this);
-        mAuth = FirebaseAuth.getInstance();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        //Button for development purposes that skips the login page
-        Button bypassBtn = (Button) findViewById(R.id.bypassBtn);
-        bypassBtn.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
-        bypassBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Home.class));
-            }
-        });
 
-        emailL = (EditText) findViewById(R.id.emailL);
-        passwordL = (EditText) findViewById(R.id.passwordL);
-        logInBtn = (Button) findViewById(R.id.loginBtnM);
-        registerBtn = (Button) findViewById(R.id.registerBtnM);
 
-        emailL.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
-        passwordL.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
-        logInBtn.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
-        registerBtn.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
+            progressDialog = new ProgressDialog(this);
+            mAuth = FirebaseAuth.getInstance();
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            //method that checks whether a user is already signed in and advanced to home activity if so
+            onStart();
+
+            //Button for development purposes that skips the login page
+            Button bypassBtn = (Button) findViewById(R.id.bypassBtn);
+            bypassBtn.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
+            bypassBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, Home.class));
+                }
+            });
+
+            emailL = (EditText) findViewById(R.id.emailL);
+            passwordL = (EditText) findViewById(R.id.passwordL);
+            logInBtn = (Button) findViewById(R.id.loginBtnM);
+            registerBtn = (Button) findViewById(R.id.registerBtnM);
+
+            emailL.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
+            passwordL.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
+            logInBtn.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
+            registerBtn.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
+
 
     }
+
+    //checking if user is signed in
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null)
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser!=null) {
+            startActivity(new Intent(MainActivity.this, Home.class));
+        }
+    }
+
+
 
     //method to open Register activity
     public void onRegister(View v) {
