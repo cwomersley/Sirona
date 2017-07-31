39da47364dc705534fcb8e4d75f6c8a02d17a65e
SJWard <samjward4@gmail.com>
2017-07-31 17:29:30 +0100

    Added functionality to notify the user when it's time to workout (currently just happens by pressing a button - not automatic yet).

--- app/src/main/java/com/example/chris/strokere/SettingsWorkoutReminder.java
+++ app/src/main/java/com/example/chris/strokere/SettingsWorkoutReminder.java
@@ -1,7 +1,12 @@
 package com.example.chris.strokere;
 
+import android.app.NotificationManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SettingsWorkoutReminder extends AppCompatActivity {
@@ -13,5 +18,31 @@ public class SettingsWorkoutReminder extends AppCompatActivity {
 
         TextView whichDays = (TextView) findViewById(R.id.whichDaysTextWR);
         whichDays.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
+
+        Button notifierBtn = (Button) findViewById(R.id.notifierBtn);
+        notifierBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                notifier();
+            }
+        });
+
+
+
     }
+
+    public void notifier() {
+        NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle("Workout Reminder")
+                .setContentText("Time to workout...");
+
+// Sets an ID for the notification
+        int mNotificationId = 001;
+// Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+// Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+
 }
