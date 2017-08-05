c92d6af0a66e8a6582412f77192558650aae6a77
aj2109 <adamj2109@gmail.com>
2017-08-05 16:41:41 +0100

    Removed the _ in the names in exercise list and capitalised first word, still need to capitalise the letters after spaces

--- app/src/main/java/com/example/chris/strokere/ExerciseList.java
+++ app/src/main/java/com/example/chris/strokere/ExerciseList.java
@@ -1,5 +1,6 @@
 package com.example.chris.strokere;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextWatcher;
 import android.view.View;
@@ -13,26 +14,44 @@ import android.widget.EditText;
 import android.text.Editable;
 import android.widget.VideoView;
 
-public class ExerciseList extends BaseActivity {
+public class ExerciseList extends AppCompatActivity {
 
     ListView listView;
     ArrayAdapter adapter;
     EditText inputSearch;
     List<String> list;
+    List<String> Headings;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_list);
-        setupNavbar();
 
 //below is populating an arraylist with the video names from raw
         Field[] f = R.raw.class.getFields();
         list = new ArrayList<>();
         for(Field fields : f)
             try{
+                //below attempts to format the text so _ are spaces (which works now) and to capitalise the first letter after
+                //the space (which does not work right now).
                 String name = fields.getName();
-                list.add(name);
+                char[] c = name.toCharArray();
+                if(name.contains("_")){
+                    while(i<name.length()) {
+                        if(c[i] == ' ') {
+                            c[i+1] = Character.toUpperCase(c[i+1]);
+                        }
+                       i++;
+                    }
+                }
+                //below if gets rid of the two unwanted field names that get added automatically
+                if(!name.equals("$change") && !name.equals("serialVersionUID")) {
+                    c[0] = Character.toUpperCase(c[0]); //make first letter always uppercase
+                    name = String.valueOf(c);
+                    name = name.replace("_", " ");
+                    list.add(name);
+                }
             }
             catch (IllegalArgumentException e) {
             }
@@ -46,15 +65,16 @@ public class ExerciseList extends BaseActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapter, View view, int p, long l) {
-
                 String name = adapter.getItemAtPosition(p).toString();
+                name = name.replace(" ", "_");
+                name = name.toLowerCase();
                 String path = "android.resource://" + getPackageName() + "//raw/"  + name;
                 VideoView v = (VideoView) findViewById(R.id.videoView2);
                 v.setVideoPath(path);
                 v.start();
-
             }
         } );
+
         //below makes the search functionality
         inputSearch = (EditText) findViewById(R.id.exerciseSearch);
         inputSearch.addTextChangedListener(new TextWatcher() {
@@ -77,10 +97,4 @@ public class ExerciseList extends BaseActivity {
 
 
     }
-
-    @Override
-    public int getLayout() {
-        return R.layout.activity_exercise_list;
-    }
-
 }
