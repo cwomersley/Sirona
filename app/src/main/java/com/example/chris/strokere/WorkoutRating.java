3c7fe87f2274bb834f834aa5a5d118f2e95c3043
Chris <christopherwomersley@gmail.com>
2017-08-16 11:46:19 +0100

    Workout rating now adds rating to server

--- app/src/main/java/com/example/chris/strokere/WorkoutRating.java
+++ app/src/main/java/com/example/chris/strokere/WorkoutRating.java
@@ -4,21 +4,34 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class WorkoutRating extends BaseActivity {
 
     private NumberPicker ratingScale;
+    private DatabaseReference mDatabase;
+    private int borgRating;
+    private String score;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_rating);
+
+         mDatabase = FirebaseDatabase.getInstance().getReference();
 
         //for nav bar
         setupNavbar();
@@ -33,11 +46,15 @@ public class WorkoutRating extends BaseActivity {
 
         finish.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                int borgRating = ratingScale.getValue();
+
+                borgRating = ratingScale.getValue();
+               mDatabase.child("BorgRatings").child(userID).child(getTime()).setValue(borgRating);
 
 
             }
         });
+
+
 
         //pushborg rating to firebase
 
@@ -45,8 +62,9 @@ public class WorkoutRating extends BaseActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
-
-
+                if(user != null) {
+                    userID = user.getUid();
+                }
             }
         });
 
@@ -56,6 +74,19 @@ public class WorkoutRating extends BaseActivity {
 
 
     }
+
+    public String getTime() {
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat time = new SimpleDateFormat("dd_MM_YY");
+        String timeString = time.format(calendar.getTime());
+
+        Log.d("Time: ", timeString);
+        return timeString;
+
+
+    }
+
     @Override
     public int getLayout () {
         return R.layout.activity_home;
