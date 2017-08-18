1da0b9e6f6bbece61133e61cf0e3fad87fcb038f
SJWard <samjward4@gmail.com>
2017-08-18 14:59:13 +0100

    Implemented being able to get data from every test type into the Graph class, and being able to select which one you want to see

--- app/src/main/java/com/example/chris/strokere/Graph.java
+++ app/src/main/java/com/example/chris/strokere/Graph.java
@@ -20,19 +20,23 @@ import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static android.graphics.Color.parseColor;
 
 public class Graph extends BaseActivity {
 
-    FirebaseDatabase myFirebaseDatabase;
-    DatabaseReference myReference;
-    String userID;
-    FirebaseUser user;
-    DataSnapshot currentSnapshot;
-    String dataDay;
-    String dataMonth;
+    private FirebaseDatabase myFirebaseDatabase;
+    private DatabaseReference myReference;
+    private String userID;
+    private FirebaseUser user;
+    private DataSnapshot currentSnapshot;
+    private String dataDay;
+    private String dataMonth;
+    private int[] sco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,31 +44,138 @@ public class Graph extends BaseActivity {
         setContentView(R.layout.activity_graph);
         setupNavbar();
 
-
         myFirebaseDatabase = FirebaseDatabase.getInstance();
         myReference = myFirebaseDatabase.getReference();
 
-        HashMap<String, Integer> sts = new HashMap<>();
-
-        sts.put("15_08_17", 9);
-        sts.put("17_08_17", 5);
-        sts.put("02_08_17", 12);
+        this.sco = new int[10];
 
         //Used documentation in http://www.android-graphview.org/simple-graph/ to construct this
 
 
+        Button sitToStandsBtn = (Button) findViewById(R.id.sitToStandsBtn);
+        sitToStandsBtn.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
+        sitToStandsBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Arrays.fill(sco, 0);
+                findScoresData(currentSnapshot, "SitToStands");
+            }
+        });
+        Button stepUpsBtn = (Button) findViewById(R.id.stepUpsBtn);
+        stepUpsBtn.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
+        stepUpsBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Arrays.fill(sco, 0);
+                findScoresData(currentSnapshot, "StepUps");
+            }
+        });
+
+        Button shuttleRun = (Button) findViewById(R.id.shuttleRunBtn);
+        shuttleRun.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
+        shuttleRun.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Arrays.fill(sco, 0);
+                findScoresData(currentSnapshot, "ShuttleRun");
+            }
+        });
+
+
+        myReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                currentSnapshot = dataSnapshot;
+                findScoresData(dataSnapshot, "SitToStands");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    userID = user.getUid();
+                }
+            }
+        });
+
+        findScoresData(currentSnapshot, "SitToStands");
+        //setupGraph();
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_graph;
+    }
+
+
+    private void findScoresData(DataSnapshot dataSnapshot, String testType) {
+
+        //Ensures this method only runs if a user is logged in
+        if (user != null) {
+
+            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                if (snapshot.getKey().equals("WorkoutTestStats")) {
+
+                    Object object = snapshot.child(userID).child(testType).getValue();
+
+                    String output = object.toString();
+
+                    Log.d("Data0: ", output);
+
+
+                    //Removes the curly brackets so that the data is consistent
+                    output = output.replace("{", ",");
+                    output = output.replace("}", ",");
+
+                    //Puts the data into an array
+                    String[] splitData = output.split(" ");
+
+                    //Splits the data into day and month pairs
+                    for (int i = 0; i < splitData.length; i++) {
+                        String part = splitData[i];
+
+                        Pattern pattern = Pattern.compile("\\=(.*?)\\,");
+                        Matcher matcher = pattern.matcher(part);
+                        while (matcher.find()) {
+                            dataDay = matcher.group(1);
+                            Log.d("Data3: ", part + "changed to: " + dataDay);
+
+                        }
+
+                        int dataInt = Integer.parseInt(dataDay);
+                        sco[i] = dataInt;
+
+
+                    }
+
+                    setupGraph();
+
+                }
+
+            }
+
+            Log.d("Data2: ", dataDay);
+
+        }
+
+    }
+
+
+    public void setupGraph() {
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
 
+        graph.removeAllSeries();
 
-        int[] sco = new int[10];
-        sco[0] = 6;
-        sco[1] = 5;
-        sco[2] = 7;
-        sco[3] = 9;
 
         DataPoint[] scores = new DataPoint[10];
-        for (int i=0; i<10; i++) {
+        for (int i = 0; i < 10; i++) {
             int ni = sco[i];
             DataPoint p = new DataPoint(i, ni);
             scores[i] = p;
@@ -73,7 +184,6 @@ public class Graph extends BaseActivity {
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(scores);
 
         graph.addSeries(series);
-
 
         series.setColor(parseColor("#4BAA71"));
         series.setThickness(6);
@@ -89,88 +199,6 @@ public class Graph extends BaseActivity {
         gridRenderer.setVerticalAxisTitleColor(parseColor("#4BAA71"));
         gridRenderer.setGridColor(parseColor("#4BAA71"));
 
-        Button btnExercise = (Button) findViewById(R.id.button2);
-        btnExercise.setTypeface(FontHelper.getLatoRegular(getApplicationContext()));
-        btnExercise.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                findScoresData(currentSnapshot);
-            }
-        });
-
-        myReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                currentSnapshot = dataSnapshot;
-                findScoresData(dataSnapshot);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-
-
-        FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                user = firebaseAuth.getCurrentUser();
-                if(user != null) {
-                    userID = user.getUid();
-                }
-            }
-        });
-
-        findScoresData(currentSnapshot);
-    }
-
-    @Override
-    public int getLayout() {
-        return R.layout.activity_graph;
-    }
-
-
-    private void findScoresData(DataSnapshot dataSnapshot) {
-
-        //Ensures this method only runs if a user is logged in
-        if(user != null) {
-
-            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-
-                if (snapshot.getKey().equals("WorkoutTestStats")) {
-
-                    Object object = snapshot.child(userID).child("SitToStands").getValue();
-
-                    String output = object.toString();
-
-                    Log.d("Data0: ", output);
-
-
-                    //Removes the initial '{' so that the data is consistent
-                    output = output.replace("{", "");
-
-                    //Puts the data into an array
-                    String[] splitData = output.split(", ");
-
-                    //Splits the data into day and month pairs
-                    for( int i = 0; i < splitData.length; i++)
-                    {
-                        String part = splitData[i];
-
-                        this.dataDay = part.substring(0, 2);
-
-                        Log.d("Data: ", dataDay);
-
-                    }
-
-                }
-
-            }
-
-            Log.d("Data2: ", dataDay);
-
-        }
 
     }
 
