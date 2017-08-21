8f9718cc9af5e5ae28c75c5e854cef9880209727
weaselsnak <edward_boyles@hotmail.com>
2017-08-21 11:29:47 +0100

    LoginTest (Instrument) now updated so can cope with a patient been signed in or out on the app

--- app/src/androidTest/java/com/example/chris/strokere/LoginTest.java
+++ app/src/androidTest/java/com/example/chris/strokere/LoginTest.java
@@ -1,10 +1,15 @@
 package com.example.chris.strokere;
 
 
+import android.content.Intent;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -22,11 +27,48 @@ import static org.hamcrest.Matchers.allOf;
 @RunWith(AndroidJUnit4.class)
 public class LoginTest {
 
+    private FirebaseUser user;
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
+
     @Test
     public void loginTest() {
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if(signedIn()) {
+            ViewInteraction appCompatButton2 = onView(
+                    allOf(withId(R.id.btnPreferencesH), withText("Settings"), isDisplayed()));
+            appCompatButton2.perform(click());
+
+            // Added a sleep statement to match the app's execution delay.
+            // The recommended way to handle such scenarios is to use Espresso idling resources:
+            // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            ViewInteraction appCompatButton3 = onView(
+                    allOf(withId(R.id.btnAccountS), withText("My Account"), isDisplayed()));
+            appCompatButton3.perform(click());
+
+            // Added a sleep statement to match the app's execution delay.
+            // The recommended way to handle such scenarios is to use Espresso idling resources:
+            // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            ViewInteraction appCompatButton4 = onView(
+                    allOf(withId(R.id.logoutBtn), withText("Logout"),
+                            withParent(allOf(withId(R.id.hiddenEmail),
+                                    withParent(withId(android.R.id.content)))),
+                            isDisplayed()));
+            appCompatButton4.perform(click());
+        }
         //logs into the app with the default username and password and then logs out
         try {
             Thread.sleep(6000);
@@ -80,6 +122,15 @@ public class LoginTest {
                         isDisplayed()));
         appCompatButton4.perform(click());
 
+    }
+
+    public boolean signedIn()
+    {
+        if (user != null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
