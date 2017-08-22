2daacfa8bd8a62c6ccc0c7d6aa7e99468dbcf8c7
weaselsnak <edward_boyles@hotmail.com>
2017-08-22 15:17:46 +0100

    Added instrument test armExTest

---
+++ app/src/androidTest/java/com/example/chris/strokere/EmailValidation.java
@@ -0,0 +1,34 @@
+package com.example.chris.strokere;
+
+/**
+ * Created by edward on 22/08/17.
+ */
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class EmailValidation {
+
+
+
+    public boolean emailValidator(String email)
+    {
+        Pattern pattern;
+        Matcher matcher;
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+        @Test
+        public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+            assertTrue(emailValidator("name@email.com"));
+        }
+
+}
