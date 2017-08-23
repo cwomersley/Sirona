1330373ae3e9e3bb3954899fa2a23db72fabde1c
Sam <samjward4@gmail.com>
2017-08-23 23:10:36 +0100

    Updated Settings page to remove unused buttons

--- app/src/androidTest/java/com/example/chris/strokere/PauseLikeBtnTest.java
+++ app/src/androidTest/java/com/example/chris/strokere/PauseLikeBtnTest.java
@@ -40,7 +40,7 @@ public class PauseLikeBtnTest {
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.loginBtnM), withText("Log in"), isDisplayed()));
         appCompatButton.perform(click());
-c
+
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
