e819a6f08a86ff8aa48894f6afcf2c162164a00c
aj2109 <adamj2109@gmail.com>
2017-08-23 15:26:37 +0100

    comment edit

--- app/src/main/java/com/example/chris/strokere/ExpandableListAdapter.java
+++ app/src/main/java/com/example/chris/strokere/ExpandableListAdapter.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * A stanadard ListView does not allow making parents/children (headers of muscle groups/each muscle groups exercises),
+ * A standard ListView does not allow making parents/children (headers of muscle groups/each muscle groups exercises),
  * this class makes an ExpandableListView to enable this.
  * @author Adam
  */
